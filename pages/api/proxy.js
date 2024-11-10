8ecafd1d7d1858f0ecacbc87cd9f65d8bbdd70df
vhratts <118541950+vratts@users.noreply.github.com>
2024-11-10 19:47:26 -0300

    Atualização do projeto: ✅ Versão 0.0.6

--- pages/api/proxy.js
+++ pages/api/proxy.js
@@ -1,23 +1,44 @@
-// pages/api/[...path].js
-
+import Cors from "cors";
 import dynamicProxy from "../../middlewares/proxy-middleware.js";
 
-export default async function handler(req, res) {
-  // Chama o middleware de proxy dinâmico
-  // Configura os headers de CORS
-  res.setHeader("Access-Control-Allow-Credentials", true);
-  res.setHeader("Access-Control-Allow-Origin", "*"); // Permite qualquer origem
-  res.setHeader("Access-Control-Allow-Methods", "GET,OPTIONS,POST,PUT");
-  res.setHeader(
-    "Access-Control-Allow-Headers",
-    "X-CSRF-Token, X-Requested-With, Accept, Authorization, Content-Type, Referer, User-Agent",
-  );
+// Inicializa o middleware CORS
+const cors = Cors({
+  origin: "*", // Permite qualquer origem
+  methods: ["GET", "OPTIONS", "POST", "PUT"],
+  allowedHeaders: [
+    "X-CSRF-Token",
+    "X-Requested-With",
+    "Accept",
+    "Authorization",
+    "Content-Type",
+    "Referer",
+    "User-Agent",
+  ],
+  credentials: true,
+});
 
-  // Para requisições OPTIONS (pré-flight), apenas retorna o status 200
+// Helper para rodar o middleware de forma assíncrona
+function runMiddleware(req, res, fn) {
+  return new Promise((resolve, reject) => {
+    fn(req, res, (result) => {
+      if (result instanceof Error) {
+        return reject(result);
+      }
+      return resolve(result);
+    });
+  });
+}
+
+export default async function handler(req, res) {
+  // Aplica o middleware CORS
+  await runMiddleware(req, res, cors);
+
+  // Lida com requisições OPTIONS (pré-flight)
   if (req.method === "OPTIONS") {
     res.status(200).end();
     return;
   }
 
+  // Chama o middleware de proxy dinâmico
   await dynamicProxy(req, res);
 }
