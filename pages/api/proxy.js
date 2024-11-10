1239a0f2a3689b0966a8a2f844c9a90e107848a2
vhratts <118541950+vratts@users.noreply.github.com>
2024-11-10 17:35:35 -0300

    Atualização do projeto: ✅ Versão 0.0.5

--- pages/api/proxy.js
+++ pages/api/proxy.js
@@ -4,5 +4,20 @@ import dynamicProxy from "../../middlewares/proxy-middleware.js";
 
 export default async function handler(req, res) {
   // Chama o middleware de proxy dinâmico
+  // Configura os headers de CORS
+  res.setHeader("Access-Control-Allow-Credentials", true);
+  res.setHeader("Access-Control-Allow-Origin", "*"); // Permite qualquer origem
+  res.setHeader("Access-Control-Allow-Methods", "GET,OPTIONS,POST,PUT");
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "X-CSRF-Token, X-Requested-With, Accept, Authorization, Content-Type, Referer, User-Agent",
+  );
+
+  // Para requisições OPTIONS (pré-flight), apenas retorna o status 200
+  if (req.method === "OPTIONS") {
+    res.status(200).end();
+    return;
+  }
+
   await dynamicProxy(req, res);
 }
