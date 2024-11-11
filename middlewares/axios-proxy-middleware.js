631306cfd8d49ff026528f4d11cae8ca453b2532
vhratts <118541950+vratts@users.noreply.github.com>
2024-11-11 14:39:00 -0300

    Atualização do projeto: ✅ Versão 0.1.4

--- middlewares/axios-proxy-middleware.js
+++ middlewares/axios-proxy-middleware.js
@@ -22,16 +22,19 @@ const customProxyMiddleware = async (req, res) => {
     // Define o corpo da requisição (para métodos POST, PUT, etc.)
     const requestData = req.method === "GET" ? {} : { data: req.body };
 
-    // Envia a requisição para o domínio de destino usando axios
-    const response = await axios({
+    const requestBodyData = {
       url: targetUrl,
       method: req.method,
       headers: { ...req.headers, host: new URL(targetDomain).host }, // Atualiza o cabeçalho 'host'
       ...requestData, // Inclui o corpo da requisição, se houver
       timeout: 60000, // Timeout configurável (60 segundos aqui)
       validateStatus: () => true, // Permite que todos os códigos de status sejam retornados
-    });
+    };
 
+    // Envia a requisição para o domínio de destino usando axios
+    const response = await axios(requestBodyData);
+
+    console.log(response.data);
     // Transfere os cabeçalhos da resposta do destino para o cliente
     Object.entries(response.headers).forEach(([key, value]) => {
       res.setHeader(key, value);
--- pages/api/[...path].js
+++ pages/api/[...path].js
@@ -44,8 +44,8 @@ export default async function handler(req, res) {
     }
 
     // Chama o middleware de proxy dinâmico
-    await dynamicProxy(req, res);
-    // await customProxyMiddleware(req, res);
+    // await dynamicProxy(req, res);
+    await customProxyMiddleware(req, res);
   } catch (error) {
     console.error("Erro no middleware CORS ou no proxy:", error);
     res.status(500).json({ error: "Erro interno no servidor." });
