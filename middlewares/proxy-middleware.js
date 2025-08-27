a09e5772f78a72fbcbe35166203588b5fbc5aae0
vhratts <o.proprio.ratts@gmail.com>
2025-08-26 21:48:49 -0300

    ⬆️  Atualizacão de suporte para requisição segura

--- middlewares/proxy-middleware.js
+++ middlewares/proxy-middleware.js
@@ -4,6 +4,7 @@ import { createProxyMiddleware } from "http-proxy-middleware";
 const dynamicProxy = async (req, res) => {
   // Obtém o domínio de destino do cabeçalho 'proxied-domain'
   const targetDomain = req.headers["proxied-domain"];
+  const isSecureContext = req.headers["is-secure-context"];
   // console.log(req, res)
   // Verifica se o cabeçalho foi fornecido e se é uma URL válida
   if (
@@ -18,12 +19,14 @@ const dynamicProxy = async (req, res) => {
     return;
   }
   console.log(
-    `Passando pelo PROXY DInamico: Redirecionando entrada para => ${targetDomain}`,
+    `Passando pelo PROXY Dinamico: Redirecionando entrada para => ${targetDomain}`,
   );
+
   // Cria um middleware de proxy dinâmico
   const proxy = createProxyMiddleware({
     target: targetDomain, // Usa o domínio do cabeçalho
     changeOrigin: true,
+    isSecureContext: isSecureContext ?? false,
     timeout: 60000, // Timeout de 60 segundos
     proxyTimeout: 60000, // Timeout para resposta do servidor de destino
     pathRewrite: { "^/api": "" }, // Remove o prefixo '/api' (opcional)
@@ -33,8 +36,8 @@ const dynamicProxy = async (req, res) => {
   return new Promise((resolve, reject) => {
     proxy(req, res, (result) => {
       if (result instanceof Error) {
-        // console.error("Erro no proxy:", result);
-        res.status(500).json({ error: "Erro ao redirecionar a requisição" });
+        console.error("Erro no proxy:", result);
+        res.status(500).json({ error: "Erro ao redirecionar a requisição", context: result });
         reject(result);
       } else {
         resolve(result);
